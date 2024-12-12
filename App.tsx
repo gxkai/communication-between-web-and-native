deb4d1c87ebc7c927155fb34146e8e14f921abf7
guxukai <gxk@33.cn>
2024-12-12 11:09:40 +0800

    updated

--- App.tsx
+++ App.tsx
@@ -1,100 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useRef} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableOpacity,
 } from 'react-native';
+import {WebView} from 'react-native-webview';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  function onMessage(data) {
+    alert(data.nativeEvent.data);
+  }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  function sendDataToWebView() {
+    webviewRef.current.postMessage('Data from React Native App');
+  }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const webviewRef = useRef();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity
+              onPress={() => sendDataToWebView()}
+              style={{
+                padding: 20,
+                width: 300,
+                marginTop: 100,
+                backgroundColor: '#6751ff',
+                alignItems: 'center',
+              }}>
+            <Text style={{fontSize: 20, color: 'white'}}>
+              Send Data To WebView / Website
+            </Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <WebView
+            ref={webviewRef}
+            scalesPageToFit={false}
+            mixedContentMode="compatibility"
+            onMessage={onMessage}
+            source={{
+              html: ` 
+          <html>
+          <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+          </head>
+          <body
+            style="
+              display: flex;
+              justify-content: center;
+              flex-direction: column;
+              align-items: center;
+            "
+          >
+            <button
+            onclick="sendDataToReactNativeApp()"
+              style="
+                padding: 20;
+                width: 200;
+                font-size: 20;
+                color: white;
+                background-color: #6751ff;
+              "
+            >
+              Send Data To React Native App
+            </button>
+            <script>
+              const sendDataToReactNativeApp = async () => {
+                window.ReactNativeWebView.postMessage('Data from WebView / Website');
+              };
+              window.addEventListener("message", message => {
+                alert(message.data) 
+              });
+            </script>
+          </body>
+        </html>        
+`,
+            }}
+        />
+      </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   sectionContainer: {
